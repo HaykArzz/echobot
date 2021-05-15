8b8481836c52efdfdbff2c538b60b73d74434978
Armen Arz <armdev@YLT190013.corp.optym.net>
2021-05-15 20:06:10 +0400

    Messege

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Python bot
 
 Starting new project
-
+add
 # helpers
 git add . 
 git commit -m "Messege"
--- bot.py
+++ bot.py
@@ -1,1 +1,24 @@
-print("Hello echo bot")
+import telebot
+import CONFIG
+
+from telebot import types
+
+bot = telebot.TeleBot(CONFIG.TOKEN)
+
+
+@bot.message_handler(commands=['ping'])
+def welcome(message):
+    print("Received chat id ", message.chat.id)
+    reply = str(message.text) + " and " + " pong to you => " + str(message.from_user.username)
+    bot.send_message(message.chat.id, reply)
+
+
+@bot.message_handler(commands=['info'])
+def info(message):
+	print("Received chat id ", message.chat.id)
+	reply = "You are :  " + str(message.from_user.username) + " " + str(message.from_user.first_name)
+	bot.send_message(message.chat.id, reply)
+
+
+
+bot.polling(none_stop=True)
