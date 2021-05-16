312705184670675169ac9998542fd5133c50654f
Armen Arz <armdev@YLT190013.corp.optym.net>
2021-05-16 17:21:14 +0400

    Messege

--- bot.py
+++ bot.py
@@ -1,24 +1,33 @@
 import telebot
 import config
-
+import dictionary
 from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
-
-@bot.message_handler(commands=['ping'])
-def welcome(message):
+@bot.message_handler(commands=['start'])
+def info(message):
     print("Received chat id ", message.chat.id)
-    reply = str(message.text) + " and " + " pong to you => " + str(message.from_user.username)
+    reply = "Please type /info country name :  "
     bot.send_message(message.chat.id, reply)
-
 
 @bot.message_handler(commands=['info'])
 def info(message):
-	print("Received chat id ", message.chat.id)
-	reply = "You are :  " + str(message.from_user.username) + " " + str(message.from_user.first_name)
-	bot.send_message(message.chat.id, reply)
-
-
+    print("Received chat id ", message.chat.id)
+    data = message.text.split(' ', 2)
+    city = dictionary.thisdict[(data[1])]
+    if city != 'null':
+      reply = "Your capital is :  " + str(city)
+    else:
+        reply = "Did not find"
+    bot.send_message(message.chat.id, reply)
 
 bot.polling(none_stop=True)
+
+
+
+
+
+
+
+
---
+++ dictionary.py
@@ -0,0 +1,9 @@
+thisdict = {
+    "Armenia": "Yerevan",
+    "France": "Paris",
+    "year": 1964
+}
+
+city = thisdict["Armenia"]
+city1 = thisdict["France"]
+
